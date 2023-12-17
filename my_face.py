31c518510fedd5cb3c930b130fb91e6b719944e7
Paul Kim <ezikial142857@gmail.com>
2023-12-18 03:17:26 +0900

    1. 인물사전 기반 등장인물 인식

--- my_face.py
+++ my_face.py
@@ -1,15 +1,30 @@
 import copy
+import os
 
 import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 from deepface import DeepFace
 
+# 데이터 디렉토리 경로
 path_data = './data/'
+
+# 감정 목록
+list_emotion = [
+    'angry',
+    'disgust',
+    'fear',
+    'happy',
+    'sad',
+    'surprise',
+    'neutral'
+]
 
 
 def identify_character(profiles, img_name):
     face_analyze_result = DeepFace.analyze(img_path=img_name,
-                                           actions=('age', 'gender', 'race', 'emotion'),
-                                           detector_backend='retinaface')
+                                           actions=('gender', 'emotion'),
+                                           detector_backend='retinaface', enforce_detection=False)
     img = cv2.imread(img_name)
 
     # 전처리
@@ -20,16 +35,6 @@ def identify_character(profiles, img_name):
     font_base_color = (0, 0, 255)
     # 지배적인 감정을 나타내는 글자 색
     font_dominant_color = (0, 255, 0)
-    # 감정 목록
-    list_emotion = [
-        'angry',
-        'disgust',
-        'fear',
-        'happy',
-        'sad',
-        'surprise',
-        'neutral'
-    ]
     # 기본 x 여백
     offset_x = 5
     # 증감 y 여백
@@ -58,9 +63,19 @@ def identify_character(profiles, img_name):
                                             distance_metric='cosine')
             print(verify_result)
             # 등장인물과 프로필의 유사도 판단 기준
-            if verify_result['distance'] <= 0.46:
+            if verify_result['distance'] <= 0.5:
                 char_name = profile.name
                 delete_index = p_index
+                for origin_profile in profiles:
+                    if origin_profile.name == profile.name:
+                        origin_profile.list_emotion.append(Emotion(round(face_result['emotion']['angry'], digit),
+                                                                   round(face_result['emotion']['disgust'], digit),
+                                                                   round(face_result['emotion']['fear'], digit),
+                                                                   round(face_result['emotion']['happy'], digit),
+                                                                   round(face_result['emotion']['sad'], digit),
+                                                                   round(face_result['emotion']['surprise'], digit),
+                                                                   round(face_result['emotion']['neutral'], digit)))
+                        break
                 break
         if delete_index is not None:
             copied_profiles.pop(delete_index)
@@ -100,31 +115,145 @@ def identify_character(profiles, img_name):
     path_array = img_name.split("/")
     dot_array = path_array[-1].split(".")
     origin_file_name = dot_array[0]
-    cv2.imwrite(f"./emotion_shots/{origin_file_name}_retina.png", img)
+    cv2.imwrite(f"./notting_hill/shot_emotion/{origin_file_name}_retina.png", img)
     # 프로그램 종료 방지, 아무 키 누르면 프로그램 종료
     # cv2.waitKey()
     # 모든 창 소멸
     # cv2.destroyAllWindows()
 
 
+class Emotion:
+    def __init__(self, angry, disgust, fear, happy, sad, surprise, neutral):
+        self.angry = angry
+        self.disgust = disgust
+        self.fear = fear
+        self.happy = happy
+        self.sad = sad
+        self.surprise = surprise
+        self.neutral = neutral
+
+
 class Character:
     def __init__(self, name, profile):
         self.name = name
         self.profile = profile
+        self.list_emotion = []
+
+
+def get_shots(path_input, alpha, limit=-1, interval=-1):
+    capture = cv2.VideoCapture(path_input)
+    prev_hist = None
+    cnt_global = 0
+    cnt_shot = 0
+    interval_frame = 0
+    # 샷 변경을 검출하기 위해 비교하는 임의의 기준값 alpha
+    if interval == -1:
+        interval_frame = int(1000 / capture.get(cv2.CAP_PROP_FPS))
+    else:
+        interval_frame = interval
+    print(f'interval_frame: {interval_frame}')
+    print()
+
+    # 최초 프레임
+    first_ret, first_frame = capture.read()
+    if first_ret:
+        prev_hist = cv2.calcHist([first_frame], [0], None, [256], [0, 256])
+        cv2.imwrite(f'./shots/{cnt_shot}.png', first_frame)
+        print(f'./shots/{cnt_shot}.png')
+        print()
+        cnt_shot += 1
+        cnt_global += 1
+
+    while True:
+        ret, frame = capture.read()
+
+        if not ret:
+            break
+
+        if limit != -1 and cnt_shot >= limit:
+            break
+
+        if cnt_global % interval_frame == 0:
+            # 샷 체인지 검출
+            curr_hist = cv2.calcHist([frame], [0], None, [256], [0, 256])
+            diff = cv2.compareHist(curr_hist, prev_hist, cv2.HISTCMP_BHATTACHARYYA)
+            if diff >= alpha:
+                cv2.imwrite(f'./shots/{cnt_shot}.png', frame)
+                print(f'diff: {diff}')
+                print(f'./shots/{cnt_shot}.png')
+                print()
+                cnt_shot += 1
+                prev_hist = curr_hist
+
+        cnt_global += 1
+
+    # 종료
+    if capture.isOpened():
+        # 사용한 자원 해제
+        capture.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    profile_list = [
-        Character('William Thacker', path_data + 'hugh_grant.png'),
-        Character('Anna Scott', path_data + 'julia_roberts.png')
+    path_profile = './notting_hill/profile/'
+    list_profile = [
+        Character('William Thacker', path_profile + 'hugh_grant.png'),
+        Character('Anna Scott', path_profile + 'julia_roberts.png'),
+        Character('Spike', path_profile + 'rhys_ifans.png'),
+        Character('Bernie', path_profile + 'hugh_bonneville.png'),
+        Character('Bella', path_profile + 'gina_mckee.png'),
+        Character('Honey', path_profile + 'emma_chambers.jpg'),
+        Character('Max', path_profile + 'tim_mcinnerny.jpg')
     ]
-    image_list = [
-        path_data + '001.png',
-        path_data + '002.png',
-        path_data + '003.png',
-        path_data + '004.png',
-        path_data + '005.png',
-        path_data + '006.png',
-    ]
-    for image in image_list:
-        identify_character(profile_list, image)
+
+    list_shot = os.listdir("./notting_hill/shot")
+    for shot in list_shot:
+        identify_character(list_profile, f'./notting_hill/shot/{shot}')
+
+    for person in list_profile:
+        sum_angry = 0
+        sum_disgust = 0
+        sum_fear = 0
+        sum_happy = 0
+        sum_sad = 0
+        sum_surprise = 0
+        sum_neutral = 0
+
+        len_emotion = len(person.list_emotion)
+        if len_emotion <= 0:
+            break
+
+        for emotion in person.list_emotion:
+            sum_angry += emotion.angry
+            sum_disgust += emotion.disgust
+            sum_fear += emotion.fear
+            sum_happy += emotion.happy
+            sum_sad += emotion.sad
+            sum_surprise += emotion.surprise
+            sum_neutral += emotion.neutral
+
+        avg_angry = sum_angry / len_emotion
+        avg_disgust = sum_disgust / len_emotion
+        avg_fear = sum_fear / len_emotion
+        avg_happy = sum_happy / len_emotion
+        avg_sad = sum_sad / len_emotion
+        avg_surprise = sum_surprise / len_emotion
+        avg_neutral = sum_neutral / len_emotion
+
+        values = [
+            avg_angry,
+            avg_disgust,
+            avg_fear,
+            avg_happy,
+            avg_sad,
+            avg_surprise,
+            avg_neutral
+        ]
+
+        x = np.arange(len(list_emotion))
+
+        plt.bar(x, values)
+        plt.xticks(x, list_emotion)
+        plt.title(f'{person.name} - graph - {len_emotion} shots')
+        plt.savefig(f'./notting_hill/graph_emotion/{person.name}_graph.png')
+        plt.clf()
